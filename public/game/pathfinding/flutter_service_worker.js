1e73f60c8601600c4b38b797968d46b730c0b79c
qizheYang <qizheYang@users.noreply.github.com>
2026-01-28 01:08:03 +0000

    Deploy pathfinding game update qizheYang/PathfindingGame_VibeCoding@dcb6674a6f080446becd0cf1750e26df19b6a5fe

--- public/game/pathfinding/flutter_service_worker.js
+++ public/game/pathfinding/flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
-"assets/fonts/MaterialIcons-Regular.otf": "2d30e53071581804236d4344a0da700e",
+"assets/fonts/MaterialIcons-Regular.otf": "6c04ffc6db7312bcc70790ff9cc185ef",
 "assets/NOTICES": "9f018f52c5cd50d3135db776ae8c96a7",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
@@ -31,9 +31,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "e566a98d908750bb2d68af21771c7100",
+"flutter_bootstrap.js": "cdfc919133eef7b79adc57c2358b729a",
 "version.json": "a61d252960818dcf87a3c839962e750d",
-"main.dart.js": "09d9926f93b9832a9ceefeb483da4cfe"};
+"main.dart.js": "ae647cdc618a8adab09beb07a8e9f963"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
